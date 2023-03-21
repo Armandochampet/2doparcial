144f7cdb1d3b6b2e1bfd3c3a9947d1868c002785
Armandochampet <armandochampet9911@gmail.com>
2023-03-21 09:59:43 -0600

    Revert "Squashed commit of the following:"
    This reverts commit 687f6e60f4aa3982188f551ac56b758d3fc77f86.

--- mayor.py
+++
@@ -1,9 +0,0 @@
-print("ingrese 2 numeros")
-n1 = int(input())
-n2 = int(input())
-if n1 < n2:
-    print("el numero mayor es: ", n2)
-elif n1 > n2:
-    print("el numero mayor es: ", n1)
-elif n1 == n2:
-    print("los numeros son iguales")
--- par.py
+++
@@ -1,6 +0,0 @@
-print("introduzca un numero")
-n1 = int(input())
-if n1%2 == 0:
-    print("es un numero par")
-else:
-    print("el numero es impar")
